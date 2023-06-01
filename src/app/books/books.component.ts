3ee005313002d9ec3ba2343ab766df8ef0020b42
Qazi-Aimal <aimalqazi32@gmail.com>
2023-06-01 12:30:10 +0500

    update the files

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,8 +7,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  status = '2';
+  status = '';
+  titles: any = [];
   input1: any;
+  bookAv: any[] = [];
   input2: any;
   issmodel = false;
   selectedOption!: string;
@@ -117,20 +119,52 @@ export class BooksComponent implements OnInit {
 
   ngOnInit(): void {
     this.availableBook = this.books.filter((book) => book.status == 0);
+    this.bookAv = this.availableBook;
 
     this.bookForm = new FormGroup({
+      id: new FormControl(''),
       book: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
       date: new FormControl('', Validators.required),
     });
+    this.bookForm.reset();
   }
 
   getting() {
     const getData = this.bookForm.getRawValue();
-
     this.bookData.push(getData);
-    this.input1 = '';
-    this.input2 = '';
+    for (let i = 0; i < this.books.length; i++) {
+      if (this.books[0].title == this.bookData[i].book) {
+        this.books[0].status = 1;
+        alert('Book is issued check the issued book list');
+      } else if (this.books[1].title == this.bookData[i].book) {
+        this.books[1].status = 1;
+      } else if (this.books[2].title == this.bookData[i].book) {
+        this.books[2].status = 1;
+      } else if (this.books[3].title == this.bookData[i].book) {
+        this.books[3].status = 1;
+      } else if (this.books[4].title == this.bookData[i].book) {
+        this.books[4].status = 1;
+      } else if (this.books[5].title == this.bookData[i].book) {
+        this.books[5].status = 1;
+      } else if (this.books[6].title == this.bookData[i].book) {
+        this.books[6].status = 1;
+      } else if (this.books[7].title == this.bookData[i].book) {
+        this.books[7].status = 1;
+      } else if (this.books[8].title == this.bookData[i].book) {
+        this.books[8].status = 1;
+      } else if (this.books[9].title == this.bookData[i].book) {
+        this.books[9].status = 1;
+      } else if (this.books[10].title == this.bookData[i].book) {
+        this.books[10].status = 1;
+      } else if (this.books[11].title == this.bookData[i].book) {
+        this.books[11].status = 1;
+      }
+      this.input1 = '';
+      this.input2 = '';
+      this.selectedOption = '';
+      this.availableBook = this.books.filter((book) => book.status == 0);
+    }
   }
 
   onOptionSelected(option: string) {
